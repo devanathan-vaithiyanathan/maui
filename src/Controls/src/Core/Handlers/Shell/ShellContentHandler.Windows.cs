8afc3f7c22f42bf92193ad7c66e9b56fff284951
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2024-09-17 16:55:09 +0530

    fix added for windows and android platform

--- src/Controls/src/Core/Handlers/Shell/ShellContentHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Shell/ShellContentHandler.Windows.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Maui.Controls.Handlers
 	public partial class ShellContentHandler : ElementHandler<ShellContent, FrameworkElement>
 	{
 		public static PropertyMapper<ShellContent, ShellContentHandler> Mapper =
-				new PropertyMapper<ShellContent, ShellContentHandler>(ElementMapper);
+				new PropertyMapper<ShellContent, ShellContentHandler>(ElementMapper) { [nameof(ShellContent.Title)] = MapTitle };
 
 		public static CommandMapper<ShellContent, ShellContentHandler> CommandMapper =
 				new CommandMapper<ShellContent, ShellContentHandler>(ElementCommandMapper);
@@ -15,6 +15,14 @@ namespace Microsoft.Maui.Controls.Handlers
 		public ShellContentHandler() : base(Mapper, CommandMapper)
 		{
 		}
+
+		public static void MapTitle(ShellContentHandler handler, ShellContent item)
++		{
++			var shellSection = item.Parent as ShellSection;
++			var shellItem = shellSection?.Parent as ShellItem;
++			var shellItemHandler = shellItem?.Handler as ShellItemHandler;
++			shellItemHandler?.UpdateTitle();
++		}
 
 		protected override FrameworkElement CreatePlatformElement()
 		{

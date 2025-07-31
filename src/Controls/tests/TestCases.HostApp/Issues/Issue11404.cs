81ec19ad96837b80596fd48b2046abd49ff8dcbe
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2025-07-31 18:49:21 +0530

    Test case added

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue11404.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 11404, "Line coordinates not computed correctly", PlatformAffected.All)]
+public class Issue11404 : ContentPage
+{
+	public Issue11404()
+	{
+		// Create a Grid
+            var grid = new Grid
+            {
+                WidthRequest = 200,
+                HeightRequest = 200,
+                BackgroundColor = Colors.LightGray
+            };
+
+            // First Line
+            var redLine = new Line
+            {
+                X1 = 0,
+                Y1 = 0,
+                X2 = 100,
+                Y2 = 100,
+                Stroke = new SolidColorBrush(Colors.Red),
+                StrokeThickness = 10
+            };
+
+            // Second Line
+            var redline = new Line
+            {
+                X1 = 200,
+                Y1 = 0,
+                X2 = 100,
+                Y2 = 100,
+                Stroke = new SolidColorBrush(Colors.Red),
+                StrokeThickness = 10
+            };
+
+            // Add lines to grid
+            grid.Children.Add(redLine);
+            grid.Children.Add(redline);
+            
+            var stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "This test checks if the lines are drawn correctly.",
+                        AutomationId = "LineCoordinatesLabel"
+                    }, 
+
+                    grid
+                }
+            };
+
+            // Set the grid as the content of the page
+        Content = stackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue11404.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue11404 : _IssuesUITest
+{
+	public override string Issue => "Line coordinates not computed correctly";
+
+	public Issue11404(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.Shape)]
+	public void VerifyLineCoOrdinatesPosition()
+	{
+		App.WaitForElement("LineCoordinatesLabel");
+		VerifyScreenshot();
+	}
+}
